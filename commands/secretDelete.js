a5d1fbc6808780dec5474f7bb6635b36251cf340
Nathaniel Inman <nate@theoestudio.com>
2019-09-02 17:23:18 -0500

    finished secret delete client-side

--- commands/secretDelete.js
+++ commands/secretDelete.js
@@ -1,5 +1,44 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+const chalk = require('chalk');
+const {User} = require('../models/User.js');
+const {prompt,confirm} = require('../libraries/prompt.js');
+
 module.exports = {
-  secretDelete(){
-    console.log('secret delete goes here.');
+  secretDelete(scopeName,secretName){
+
+    // short-circuit failure
+    if(!fs.existsSync('./user.json')){
+      return console.log(chalk.red('No user exists locally, initialize first with: ')+chalk.cyan('broker init'));
+    } //end if
+    const user = new User(JSON.parse(fs.readFileSync('./user.json')));
+
+    try{
+      const data = await fetch(`${user.remoteIP}/secretDelete`,{
+              method: 'POST',
+              body: JSON.stringify({
+                key: fs.readFileSync(user.pgpPrivateKeyLocation).toString(),
+                name: user.name,
+                scopeName,
+                secretName
+              }),
+              headers: {
+                'Content-Type': 'application/json',
+                name: user.name,
+                email: user.email
+              }
+            })
+            .then(res=> res.json())
+            .then(res=>{
+              if(res.success){
+                console.log(`Secret "${name}" deleted successfully!`);
+              }else{
+                console.log(res.error);
+              } //end if
+            });
+    }catch(err){
+      console.log('Problem connecting to server.');
+      console.log(err);
+    }
   }
 };
