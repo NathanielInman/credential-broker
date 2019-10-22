7a7e4f1af0667db0642935caa612f0bdb0b4f52c
Nathaniel Inman <nate@theoestudio.com>
2019-10-22 12:29:32 -0500

    build process is more simple

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
-mkdir -p ./built/mac
+mkdir -p ./built
 nexe ./index.js -t ./mac-x64-12.9.1
-mv ./credential-broker ./built/mac/broker
-mkdir -p ./built/linux
+mv ./credential-broker ./built/broker-mac-x64
 nexe ./index.js -t ./linux-x64-12.9.1
-mv ./credential-broker ./built/linux/broker
-mkdir -p ./built/windows
+mv ./credential-broker ./built/broker-linux-x64
 nexe ./index.js -t ./windows-x64-12.9.1
-mv ./credential-broker ./built/windows/broker
+mv ./credential-broker ./built/broker-windows-x64
